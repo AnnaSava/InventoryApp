607efa945f0977b8bef71ea6ac46c532b51d4401
Anna Savina <anna.s.savina@gmail.com>
2018-08-06 08:20:31 +0300

    Insert and Read

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -1,13 +1,109 @@
 package com.example.android.inventoryapp;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.android.inventoryapp.data.InventoryDbHelper;
+import com.example.android.inventoryapp.data.InventoryContract.ProductEntry;
 
 public class MainActivity extends AppCompatActivity {
+
+    private InventoryDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDbHelper = new InventoryDbHelper(this);
+
+        insertProduct();
+        displayDatabaseInfo();
+    }
+
+    private void insertProduct() {
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(ProductEntry.COLUMN_PRODUCT_NAME, "Barbie Basics #1");
+        values.put(ProductEntry.COLUMN_PRODUCT_PRICE, 14.95);
+        values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, 100);
+        values.put(ProductEntry.COLUMN_PRODUCT_SUPPLIER_NAME, "Mattel");
+        values.put(ProductEntry.COLUMN_PRODUCT_SUPPLIER_PHONE, "+000 111 22 33");
+
+        long newRowId = db.insert(ProductEntry.TABLE_NAME, null, values);
+    }
+
+    private void displayDatabaseInfo() {
+
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        String[] projection = {
+                ProductEntry._ID,
+                ProductEntry.COLUMN_PRODUCT_NAME,
+                ProductEntry.COLUMN_PRODUCT_PRICE,
+                ProductEntry.COLUMN_PRODUCT_QUANTITY,
+                ProductEntry.COLUMN_PRODUCT_SUPPLIER_NAME,
+                ProductEntry.COLUMN_PRODUCT_SUPPLIER_PHONE
+        };
+
+        // Perform this raw SQL query "SELECT * FROM products"
+        // to get a Cursor that contains all rows from the products table.
+        Cursor cursor = db.query(ProductEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        // Display the number of rows in the Cursor (which reflects the number of rows in the
+        // products table in the database).
+        TextView displayView = (TextView) findViewById(R.id.text_view_product);
+
+        try {
+            displayView.setText("The products table contains " + cursor.getCount() + " product(s).\n\n");
+            displayView.append(ProductEntry._ID + " - " +
+                    ProductEntry.COLUMN_PRODUCT_NAME + " - " +
+                    ProductEntry.COLUMN_PRODUCT_PRICE + " - " +
+                    ProductEntry.COLUMN_PRODUCT_QUANTITY + " - " +
+                    ProductEntry.COLUMN_PRODUCT_SUPPLIER_NAME + " - " +
+                    ProductEntry.COLUMN_PRODUCT_SUPPLIER_PHONE + "\n");
+            // Figure out the index of each column
+            int idColumnIndex = cursor.getColumnIndex(ProductEntry._ID);
+            int nameColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_NAME);
+            int priceColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_PRICE);
+            int quantityColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_QUANTITY);
+            int supplierNameColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_SUPPLIER_NAME);
+            int supplierPhoneColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_SUPPLIER_PHONE);
+            // Iterate through all the returned rows in the cursor
+            while (cursor.moveToNext()) {
+                // Use that index to extract the String or Int value of the word
+                // at the current row the cursor is on.
+                int currentID = cursor.getInt(idColumnIndex);
+                String currentName = cursor.getString(nameColumnIndex);
+                float currentPrice = cursor.getFloat(priceColumnIndex);
+                int currentQuantity = cursor.getInt(quantityColumnIndex);
+                String currentSupplierName = cursor.getString(supplierNameColumnIndex);
+                String currentSupplierPhone = cursor.getString(supplierPhoneColumnIndex);
+                // Display the values from each column of the current row in the cursor in the TextView
+                displayView.append(("\n" + currentID + " - " +
+                        currentName + " - " +
+                        currentPrice + " - " +
+                        currentQuantity + " - " +
+                        currentSupplierName + " - " +
+                        currentSupplierPhone));
+            }
+
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
     }
 }
